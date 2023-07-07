98008cb797a74ff5a2de2b32493db23f8c59dfa2
mariamolgeorge123 <mariadaisy1990@gmail.com>
2023-07-06 21:26:07 -0400

    add one more file

---
+++ com/controlStatements/oneclass.java
@@ -0,0 +1,20 @@
+package com.controlStatements;
+
+import java.util.Scanner;
+
+public class oneclass {
+
+    public static void main(String[] args) {
+//        int num=-5;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number");
+        int num=sc.nextInt();
+        if(num<0)
+        {
+            System.out.println("it is negative number");
+        }
+        else {
+            System.out.println("it is positive number");
+        }
+    }
+}
